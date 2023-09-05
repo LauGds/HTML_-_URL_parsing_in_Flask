a9949984f7ed42b62038ecd12cf7f8bbc9db0b56
LauGds <137927575+LauGds@users.noreply.github.com>
2023-09-05 11:51:15 -0700

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,97 @@
+from flask import Flask
+
+app = Flask(__name__)
+print(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return '<h1 style="text-align: center">Hello, World!</h1>' \
+           '<p>This is a paragraph.</p>' \
+           '<img src="https://media.giphy.com/media/dZOjGQk3obX4smrQRj/giphy-downsized-large.gif">'
+
+
+# Different routes using the app.route decorator
+def make_bold(function):
+    def wrapper():
+        return "<b>" + function() + "</b>"
+
+    return wrapper
+
+
+def make_emphasis(function):
+    def wrapper():
+        return "<em>" + function() + "</em>"
+
+    return wrapper
+
+
+def make_underlined(function):
+    def wrapper():
+        return "<u>" + function() + "</u>"
+
+    return wrapper
+
+
+@app.route('/bye')
+@make_bold
+@make_emphasis
+@make_underlined
+def bye():
+    return 'Bye'
+
+
+# Creating variable paths and converting the path to a specified data type
+@app.route('/username/<name>/<int:number>')
+def greet(name, number):
+    return f"Hello {name} and you are {number} years old!"
+
+
+if __name__ == "__main__":
+    # Run the app in debug mode to auto-reload
+    app.run(debug=True)
+
+
+# Advanced Python Decorator Functions
+
+class User:
+    def __init__(self, name):
+        self.name = name
+        self.is_logged_in = False
+
+
+def is_authenticated_decorator(function):
+    def wrapper(*args, **kwargs):
+        if args[0].is_logged_in == True:
+            function(args[0])
+    return wrapper
+
+
+@is_authenticated_decorator
+def create_blog_post(user):
+    print(f"This is {user.name}'s new blog post.")
+
+
+new_user = User("laura")
+new_user.is_logged_in = True
+create_blog_post(new_user)
+
+
+
+
+def logging_decorator(function):
+    def wrapper(*args, **kwargs):
+        print(f"You called {function.__name__}{args}")
+        result = function(args[0], args[1], args[2])
+        print(f"It returned: {result}")
+    return wrapper
+
+
+@logging_decorator
+def a_function(n1, n2, n3):
+    return n1 + n2 + n3
+
+
+a_function(2, 4, 6)
+
+
